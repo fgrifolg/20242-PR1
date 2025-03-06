cf5b12e6b31a36e62ef98f3dae8a8fcb2d4c13a3
ksarasola-uoc <128643155+ksarasola-uoc@users.noreply.github.com>
2025-03-06 09:18:29 +0100

    Primer commit, estructura de la PRA-1

---
+++ pra1_estructura.js
@@ -0,0 +1,56 @@
+class Film {
+    constructor(id, title, overview, popularity, poster_path, release_date, vote_average, vote_count, genre_ids) {
+
+    }
+
+    //Getters y setters
+
+
+}
+
+class FilmList {
+    constructor() {
+        
+    }
+
+    addFilm (film){
+
+    };
+    
+
+    removeFilm (filmId) {
+        
+    }
+
+    showList() {
+        
+    }
+
+    addMultipleFilms = (...films) => {
+
+    }
+    
+
+    getFilmsByDateRange = (startDate, endDate) => {
+
+    }
+
+    sortFilmsByPopularity = () => {
+
+    }
+    
+    findFilmById() {
+        //Función recursiva
+    }
+    
+    getMostCommonGenre() {
+        //Uso de reduce
+    }
+
+    getPopularFilmTitles() {
+        //Uso de map y filter
+    }
+}
+
+
+
