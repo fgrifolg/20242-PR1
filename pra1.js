205302f8fccecf9805f5d61d883db8fd1e61965f
fgrifolg <fgrifolg@uoc.edu>
2025-04-09 22:33:17 +0200

    Empezando con el constructor

--- pra1.js
+++ pra1.js
@@ -1,10 +1,23 @@
-//comentario: Ejercicio 1
+// Clase Film -- Película con su propiedades
 class Film {
     constructor(id, title, overview, popularity, poster_path, release_date, vote_average, vote_count, genre_ids) {
-
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.poster_path = poster_path;
+        this.release_date = release_date;
+        this.vote_average = vote_average;
+        this.vote_count = vote_count;
+        this.genre_ids = genre_ids;
     }
 
     //Getters y setters
+
+    getId() { return this.id; }
+    setId(id) { this.id = id; }
+    getTitle() { return this.title; }
+    setTitle(title) { this.title = title; }
 
 }
 
